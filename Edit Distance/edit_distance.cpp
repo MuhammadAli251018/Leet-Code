699da5ad79deb43e0ad61cb1bf0f52934fb9c6fc
Muhammad <muhammadali251018@gmail.com>
2023-02-26 19:39:25 +0200

    created the the main file

---
+++ Edit Distance/edit_distance.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+
+public:
+    int minDistance(string word1, string word2) 
+    {
+    cout << "minDistancet"<< endl;;
+     return 0;   
+    }
+
+};
+
+int main() 
+{
+    //Solution solution = Solution();
+    cout << "Hello World!" << endl;
+    return 0;
+}
